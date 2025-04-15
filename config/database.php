5f514fbd3eabd6c5d2f1f3f52124f1ccaab6c7ef
Ricardo Nelson <sup.ricardo@gmail.com>
2025-04-15 10:17:01 -0300

    Atualização k302js

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
 require_once 'env.php';
-carregarEnv(__DIR__ . '/.env');
+carregarEnv(__DIR__ . '/../.env');
 $dsn = 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME') . ';charset=utf8mb4';
 try {
-$pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'));
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-exit('Erro ao conectar: ' . $e->getMessage());
+    $pdo = new PDO($dsn, getenv('DB_USER'), getenv('DB_PASS'));
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $e) {
+    exit('Erro ao conectar: ' . $e->getMessage());
 }
 ?>
